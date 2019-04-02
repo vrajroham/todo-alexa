61086ca360b7911c5c423e319865c0a7b1d2f1a5
Vaibhavraj Roham <vaibhavraj.developer@gmail.com>
2019-04-02 11:44:50 +0530

    Added frontend and anew route to access passport dashboard

---
+++ resources/views/home-oauth.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <passport-clients></passport-clients>
+            <br>
+            <passport-authorized-clients></passport-authorized-clients>
+            <br>
+            <passport-personal-access-tokens></passport-personal-access-tokens>
+        </div>
+    </div>
+</div>
+@endsection 
